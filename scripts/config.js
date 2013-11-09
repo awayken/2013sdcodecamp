e07a36c821fc9e87a5cae461efe9dbead8d720e4
Miles Rausch <miles@milesrausch.com>
2013-11-09 09:59:59 -0600

    Tweaks for offline necessity.

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,8 @@
 var requirejs = {
     'paths': {
-        'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-        'jquery.validate': '//ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min'
+        'jquery': [
+            '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+            '/scripts/jquery.min'
+        ]
     }
 };
